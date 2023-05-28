78c5a4af82f44484c75071eb58420f712388a051
Pavan Srinivas Mamidala <chinni.mamidala@gmail.com>
2023-05-28 17:44:46 -0400

    build bugs

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
 /* eslint-disable no-unused-vars */
+
 import adapter from '@sveltejs/adapter-vercel';
 // import adapter from '@sveltejs/adapter-auto';
 import { Server } from 'socket.io';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// // Override http methods in the Todo forms
-		// methodOverride: {
-		// 	allowed: ['PATCH', 'DELETE']
-		// }
+		adapter: adapter({
+			runtime: 'nodejs18.x',
+		  }),
 	}
 };
 
