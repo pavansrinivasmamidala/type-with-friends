de8e37a38a8a191310d76363138e4d29e6f60e96
Pavan Srinivas Mamidala <chinni.mamidala@gmail.com>
2023-05-28 18:02:16 -0400

    Revert "build bugs"
    This reverts commit 78c5a4af82f44484c75071eb58420f712388a051.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
-
 import adapter from '@sveltejs/adapter-vercel';
 // import adapter from '@sveltejs/adapter-auto';
 import { Server } from 'socket.io';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs18.x',
-		  }),
+		adapter: adapter(),
+
+		// // Override http methods in the Todo forms
+		// methodOverride: {
+		// 	allowed: ['PATCH', 'DELETE']
+		// }
 	}
 };
 
